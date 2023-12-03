396bb0a0ab1a2485bdb045db2ab308a6703f0b73
Younes Harrat <harratanis@hotmail.fr>
2023-12-03 18:48:45 +0100

    update portfolio

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,14 +1,13 @@
-<script>
-
-</script>
 # Portfolio
 
 <br>
 
 <ul>
+    <li><a href="/portfolio/competences">Competences</a></li>
     <li><a href="/portfolio/morpion">TicTacToe</a></li>
     <!-- <li><a href="/portfolio/snake">Snake</a></li> -->
-    <li><a href="/portfolio/competences">Competences</a></li>
+    <li><a href="https://sample-pro-btp.vercel.app/" target="_blank">ChartsJS</a></li>
+    <li><a href="https://weather-map-pfftxdcfs-younesharrat.vercel.app/" target="_blank">WeatherMapJS</a></li> 
     <!-- <li><a href="/portfolio/animations">animations</a></li> -->
 </ul>
 
